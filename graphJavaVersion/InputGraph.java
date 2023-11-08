0d2ff159b8df7d7ddc3a44aebcba7d49567cc5b4
= <mukherjee4004@gmail.com>
2023-11-08 20:57:16 +0530

    add taking a graph input in matrix form

---
+++ graphJavaVersion/InputGraph.java
@@ -0,0 +1,37 @@
+package graphJavaVersion;
+import java.util.Scanner;
+
+public class InputGraph {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of vertices in the graph:");
+        int numVertices = scanner.nextInt();
+
+        System.out.println("Enter the number of edges in the graph:");
+        int numEdges = scanner.nextInt();
+
+        int[][] adjacencyMatrix = new int[numVertices][numVertices];
+
+        for (int i = 0; i < numEdges; i++) {
+            System.out.println("Enter the source vertex for edge " + (i + 1) + ":");
+            int sourceVertex = scanner.nextInt();
+
+            System.out.println("Enter the destination vertex for edge " + (i + 1) + ":");
+            int destinationVertex = scanner.nextInt();
+
+            adjacencyMatrix[sourceVertex][destinationVertex] = 1;
+            adjacencyMatrix[destinationVertex][sourceVertex] = 1; // For undirected graphs
+        }
+
+        System.out.println("The adjacency matrix is:");
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                System.out.print(adjacencyMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
--- graphJavaVersion/Main.java
+++
@@ -1,6 +0,0 @@
-package graphJavaVersion;
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-}
