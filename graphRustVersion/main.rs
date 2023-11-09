21a8c108d396283053337cf40948d074116f00f1
= <mukherjee4004@gmail.com>
2023-11-09 21:01:07 +0530

    add readme file for rust and question 1

--- graphRustVersion/main.rs
+++ graphRustVersion/main.rs
@@ -1,18 +1,38 @@
-#[cfg(target_os = "linux")] 
-fn checking_on_linux() { 
-    println!("Currently on linux!"); 
-} 
-  
-#[cfg(not(target_os = "linux"))] 
-fn checking_on_linux() { 
-    println!("Not on linux!"); 
-} 
-  
-fn main() { 
-   checking_on_linux(); 
-    if cfg!(target_os = "linux") { 
-        println!("Running on linux"); 
-    } else { 
-        println!("Not Running on linux"); 
-    } 
-} 
+use std::io;
+
+fn main() {
+    // Read the number of vertices in the graph
+    println!("Enter the number of vertices:");
+    let mut num_vertices = String::new();
+    io::stdin().read_line(&mut num_vertices).expect("Failed to read line");
+    let num_vertices: usize = num_vertices.trim().parse().expect("Invalid input for number of vertices");
+
+    // Initialize an empty adjacency matrix
+    let mut adjacency_matrix = vec![vec![0; num_vertices]; num_vertices];
+
+    // Read the adjacency matrix values
+    println!("Enter the adjacency matrix (row by row):");
+    for i in 0..num_vertices {
+        let mut row = String::new();
+        io::stdin().read_line(&mut row).expect("Failed to read line");
+        let row_values: Vec<u32> = row
+            .split_whitespace()
+            .map(|s| s.parse().expect("Invalid input for adjacency matrix"))
+            .collect();
+
+        if row_values.len() != num_vertices {
+            panic!("Invalid number of values in the row. Should be equal to the number of vertices.");
+        }
+
+        adjacency_matrix[i] = row_values;
+    }
+
+    // Display the adjacency matrix
+    println!("Adjacency Matrix:");
+    for row in &adjacency_matrix {
+        for value in row {
+            print!("{:4} ", value);
+        }
+        println!();
+    }
+}
---
+++ graphRustVersion/readme.md
@@ -0,0 +1,20 @@
+## 1 . Graph as input in the form of an adjacency matrix
+
+- We first read the number of vertices in the graph from the user.
+
+- We then create an empty adjacency matrix as a 2D vector with all values initialized to 0.
+```cpp
+let mut adjacency_matrix = vec![vec![0; num_vertices]; num_vertices];
+```
+- Next, we read the adjacency matrix values row by row from the user. We split each row's input into space-separated values, parse them into u32, and store them in the adjacency matrix.
+```cpp
+ let row_values: Vec<u32> = row
+            .split_whitespace()
+            .map(|s| s.parse().expect("Invalid input for adjacency matrix"))
+            .collect();
+```
+
+- Finally, we display the adjacency matrix to verify that it has been correctly input.
+
+
+![Animated Gif](https://lh6.googleusercontent.com/RJXRgXqBBveetZajhikyA8q29MhMzKKDUXH0WuQb9-k7owhUIi6rZxqvyU-gNicg7HqRYHOE44_C5ojbCpQlbi0QDIn2ZuwWUA6LD5wYSWz6ehdDthINzqofzZ5NZJyEbShmYhJmn7j7IZFlhvyh_uY)
